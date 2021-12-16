11dd7d3a6a6736b8349e7eb549b0e83aead8f7a9
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-12-16 14:25:43 -0500

    Inside of MeetupList.js file i am passing in a prop of meetups and then i am mapping over the meetups prop and returning the meetupItem component with key, id and the props i passed into the MeetupItem component

--- meetup/src/components/meetuplist/MeetupList.js
+++ meetup/src/components/meetuplist/MeetupList.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import MeetupItem from '../meetupitem/MeetupItem'
+import './MeetupList.css'
 
-const MeetupList = () => {
+const MeetupList = ({ meetups }) => {
   return (
-    <div>
-
-    </div>
+    <ul className='list'>
+      {meetups.map((meetup) => {
+        return <MeetupItem key={meetup.id} id={meetup.id} title={meetup.title} image={meetup.image} address={meetup.address} description={meetup.description} />
+      })}
+    </ul>
   )
 }
 
