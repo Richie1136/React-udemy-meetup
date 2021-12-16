fe1c884ecb3aa2627d1d9b522933e03e10dc3709
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-12-16 14:16:07 -0500

    Added styling to my MeetupItem.js file

--- meetup/src/components/meetupitem/MeetupItem.js
+++ meetup/src/components/meetupitem/MeetupItem.js
@@ -1,17 +1,18 @@
 import React from 'react'
+import './MeetupItem.css'
 
 const MeetupItem = ({ title, image, address, description }) => {
   return (
-    <li>
-      <div>
+    <li className='item'>
+      <div className='image'>
         <img src={image} alt={title} />
       </div>
-      <div>
+      <div className='content'>
         <h3>{title}</h3>
         <address>{address}</address>
         <p>{description}</p>
       </div>
-      <div>
+      <div className='actions'>
         <button>To Favorites</button>
       </div>
     </li>
