0979a3a5dbd59579593f000c61f159ed36be0172
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-12-17 13:22:38 -0500

    Sending a fetch request of 'GET' method to retreieve the inputted data from the backend, using the .then to read the data by using the json method, added a second .then block to get the data, added state with a loading state to return a Loading message when the data is loading before i return and return the meetuplist if we are done loading, added a loadedMeetups piece of state which is initially set to an empty array but i then set the state updating function to the data and pass the state function as the value to the meetups prop

--- meetup/src/pages/AllMeetups.js
+++ meetup/src/pages/AllMeetups.js
@@ -1,35 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MeetupList from '../components/meetuplist/MeetupList'
 
-const DUMMY_DATA = [
-  {
-    id: 'm1',
-    title: 'A first meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Zion_Williamson_2020.jpg/800px-Zion_Williamson_2020.jpg',
-    address: 'Some address 5, 32456',
-    description: 'This the first'
-  },
-  {
-    id: 'm2',
-    title: 'A second meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/c/cc/Jonathan_Taylor.png',
-    address: 'Some address 15, 32456',
-    description: 'This the second'
-  },
-  {
-    id: 'm3',
-    title: 'A third meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Aaron_Judge_2018.jpg/1920px-Aaron_Judge_2018.jpg',
-    address: 'The address 15, 32456',
-    description: 'This the third'
-  }
-]
 
 const AllMeetupsPage = () => {
+  const [loading, setLoading] = useState(true)
+  const [loadedMeetups, setLoadedMeetups] = useState([])
+
+  fetch('https://react-meetups-175c9-default-rtdb.firebaseio.com/meetups.json')
+    .then(res => res.json())
+    .then(data => {
+      setLoading(false)
+      setLoadedMeetups(data)
+    })
+
+  if (loading) {
+    return <p>Loading...</p>
+  }
+
   return (
     <div>
       <h1>All Meetups</h1>
-      <MeetupList meetups={DUMMY_DATA} />
+      <MeetupList meetups={loadedMeetups} />
     </div>
   )
 }
